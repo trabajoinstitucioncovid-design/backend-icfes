76688327a01c237ee1aad9bf87b2c2172380b3eb
Institucion Covid <trabajoinstitucioncovid@gmail.com>
2026-01-30 20:20:55 -0500

    Agregando acceso a archivos html

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,10 @@ const PORT = process.env.PORT || 5000;
 const dbConnection = process.env.MONGO_URI || process.env.MONGODB_URI;
 
 // Middleware
+// Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static(__dirname)); // <--- ¡ESTA ES LA LÍNEA NUEVA!
 app.use(express.urlencoded({ extended: true }));
 
 // Conexión a MongoDB
